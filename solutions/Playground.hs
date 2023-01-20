618ba1b3888634762fc14c7a27f80ae3a2a4c951
darjanf <d.fikic@icloud.com>
2023-01-20 11:17:58 +0000

    Firstshit A1.hs (A1 Assignment) Added Playground.hs for testing purpose

---
+++ solutions/Playground.hs
@@ -0,0 +1,16 @@
+module A1 where
+
+import Data.Char (toUpper)
+
+_MY_PAIR_ :: (Int, Bool)
+_MY_PAIR_ = (33, True)
+
+_TRIPLE_ :: (Int, Bool, Char)
+_TRIPLE_ = (22, False, 'j')
+
+_FIB_ :: [Int]
+_FIB_ = [33,22, 11]
+
+_FAV_POKEMON_ :: String
+_FAV_POKEMON_ = "Alakazam"
+
