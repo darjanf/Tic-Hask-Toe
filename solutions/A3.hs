ccf6be9f01db88edcb2ce34c38776c67e364502d
darjanf <d.fikic@icloud.com>
2023-02-08 15:04:48 +0000

    added solutons for assignment A3-1.md

--- solutions/A3.hs
+++ solutions/A3.hs
@@ -4,45 +4,60 @@ import A1
 import A2
 
 import Data.List (transpose)
+import Data.Monoid (All(getAll))
 
 -- *** Assignment 3-1 ***
 
 -- Q#01
+showInts :: [Int] -> [String]
+showInts [] = []
+showInts (x:xs) = show x : showInts xs
 
-showInts = undefined
-
-
-_HEADER_ = undefined
+_HEADER_' :: String
+_HEADER_' = " " ++ formatLine (showInts _RANGE_)
 
 -- Q#02
-
-showSquares = undefined
+showSquares' :: [Square] -> [String]
+showSquares' []     = []
+showSquares' (x:xs) = show x : showSquares' xs
 
 
 -- Q#03
-
-formatRows = undefined
+formatRows' :: [Row] -> [String]
+formatRows' [] = []
+formatRows' (x:xs) = formatLine (showSquares' x) : formatRows' xs
 
 -- Q#04
+isColEmpty :: Row -> (Int -> Bool)
+isColEmpty [] _     = False
+isColEmpty (x:_) 0  = x == EMPTY
+isColEmpty (_:xs) i = isColEmpty xs (i - 1)
 
-isColEmpty = undefined
+isColEmpty' :: Row -> (Int -> Bool)
+isColEmpty' [] _ = False
+isColEmpty' r i = isInBound i && y == EMPTY where (x,y:ys)= splitAt i r
 
 -- Q#05
+dropFirstCol' :: Board -> Board
+dropFirstCol' []      = []
+dropFirstCol' (x:xs)  = tail x : dropFirstCol' xs
 
-dropFirstCol = undefined
-
-
-dropLastCol = undefined
+dropLastCol' :: Board -> Board
+dropLastCol' []     = []
+dropLastCol' (x:xs) = init x : dropLastCol' xs
 
 -- Q#06
+getDiag1 :: Board -> Line
+getDiag1 [] = []
+getDiag1 (x:xs) = head x : getDiag1 (dropFirstCol' xs)
 
-getDiag1 = undefined
+getDiag2 :: Board -> Line
+getDiag2 [ ]= []
+getDiag2 (x:xs) = last x : getDiag2 (dropLastCol' xs)
 
-
-getDiag2 = undefined
-
-
-getAllLines = undefined
+getAllLines :: Board -> [Line]
+getAllLines [] = []
+getAllLines b = concat [b, transpose b, [getDiag1 b], [getDiag2 b]]
 
 -- *** Assignment 3-2 ***
 
