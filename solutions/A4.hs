929120fb39864f398ba9b5d586adb0b89d5e84e6
darjanf <d.fikic@icloud.com>
2023-02-09 13:20:11 +0000

    added module Playground to Main.hs added solutions from A4-1.md to A4.hs

--- solutions/A4.hs
+++ solutions/A4.hs
@@ -15,29 +15,30 @@ import A3 hiding (
 -- *** Assignment 4-1 *** --
 
 -- Q#01
-
-_HEADER_ = undefined
+_HEADER_ :: String
+_HEADER_ = " " ++ formatLine (map show _RANGE_)
 
 -- Q#02
-
-showSquares = undefined
+showSquares :: [Square] -> [String]
+showSquares sq = map show sq
 
 -- Q#03
-
-dropFirstCol = undefined
+dropFirstCol :: Board -> Board
+dropFirstCol b = map tail b
 
 -- Q#04
-
-dropLastCol = undefined
+dropLastCol :: Board -> Board
+dropLastCol b = map init b
 
 --Q#05
-
-formatRows = undefined
+formatRows :: [Row] -> [String]
+formatRows rl = map (\x -> formatLine (showSquares x)) rl
+--formatRows rl = map (formatLine . showSquares) rl
 
 -- Q#06
-
-isWinningLine_ = undefined
-
+isWinningLine_ :: Player -> (Line -> Bool)
+isWinningLine_ _ [] = False
+isWinningLine_ p l = null (filter (\sq -> p /= sq) l)
 
 -- *** Assignment 4-2 *** --
 
